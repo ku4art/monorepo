d84fa834685fd48b7bc779663a658f7e63043df7
Alexandr <alexfuertevip@gmail.com>
2025-12-28 20:15:08 +0500

    feat(database): we use superjson to get the date in js format

--- apps/api/src/lib/trpc.ts
+++ apps/api/src/lib/trpc.ts
@@ -1,12 +1,15 @@
 import { initTRPC } from '@trpc/server'
 import * as trpcExpress from '@trpc/server/adapters/express'
 import { type Express } from 'express'
+import SuperJSON from 'superjson'
 
 import { type TrpcRouter } from '@/router'
 
 import { AppContext } from './ctx'
 
-export const trpc = initTRPC.context<AppContext>().create()
+export const trpc = initTRPC.context<AppContext>().create({
+  transformer: SuperJSON,
+})
 
 export const applyTrpcToExpressApp = (expressApp: Express, appContext: AppContext, trpcRouter: TrpcRouter) => {
   expressApp.use(
